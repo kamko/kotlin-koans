64effe0c0bd70e2c9492e98f8e7684a2cb62a503
Kamil <j@necek.com>
2018-06-22 21:43:36 +0200

    i_introduction

--- src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
+++ src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
@@ -25,7 +25,12 @@ fun todoTask7(client: Client?, message: String?, mailer: Mailer): Nothing = TODO
 fun sendMessageToClient(
         client: Client?, message: String?, mailer: Mailer
 ) {
-    todoTask7(client, message, mailer)
+    val personalInfo = client?.personalInfo
+    val mail = personalInfo?.email
+
+    if (mail != null && message != null) {
+        mailer.sendMessage(mail, message)
+    }
 }
 
 class Client (val personalInfo: PersonalInfo?)
